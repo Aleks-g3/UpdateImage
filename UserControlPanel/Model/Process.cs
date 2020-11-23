1607a50dde59bb6f0008a7a7a9b7b426bb172307
kleszczalex <kleszczalex@gmail.com>
2020-11-23 10:07:43 +0100

    init gui

---
+++ UserControlPanel/Model/Process.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserControlPanel.Model
+{
+    public class Process: INotifyPropertyChanged
+    {
+        private string imageFile;
+
+        public string ImageFile
+        {
+            get
+            {
+                return imageFile;
+            }
+            set
+            {
+                if (imageFile != value)
+                {
+                    imageFile = value;
+                    RaisePropertyChanged("ImageFile");
+                }
+
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
---
+++ UserControlPanel/ViewModel/ProcessViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpdateImage;
+using UserControlPanel.Model;
+
+namespace UserControlPanel.ViewModel
+{
+    public class ProcessViewModel
+    {
+        private ImageProcessing imageProcessing = new ImageProcessing();
+        private Process process = new Process();
+        System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+        private TimeSpan timeTaken;
+
+        public Process Process
+        {
+            get;
+            set;
+        }
+        public void LoadImage(string path, string imageFile)
+        {
+            timer.Restart();
+            timer.Start();
+            process.ImageFile = imageProcessing.ToMainColors(path, imageFile);
+            timer.Stop();
+            timeTaken = timer.Elapsed;
+            MessageBox.Show("Czas wykonania : " + timeTaken.ToString(@"m\:ss\.fff"), "OK");
+
+        }
+        public void LoadImageAsync(string path, string imageFile)
+        {
+            timer.Restart();
+            timer.Start();
+            process.ImageFile = Task.Run(async () => await imageProcessing.ToMainColorsAsync(path, imageFile)).Result;
+            timer.Stop();
+            timeTaken = timer.Elapsed;
+            MessageBox.Show("Czas wykonania : " + timeTaken.ToString(@"m\:ss\.fff"), "OK");
+        }
+    }
